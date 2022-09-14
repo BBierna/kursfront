5818ff2cdd529507aaad45d193a5bcbfb492ac01
BBierna <bbierna.bary@gmail.com>
2022-09-14 17:38:21 +0200

    gra

---
+++ gra/scripts.js
@@ -0,0 +1,17 @@
+
+const gamePlane = document.querySelector('.gamePlane')
+
+
+const wall = document.createElement('div')
+wall.style.cssText = `
+    background:green;
+    width:20%;
+    height:20%;
+    left:40%`
+
+
+
+
+
+
+
